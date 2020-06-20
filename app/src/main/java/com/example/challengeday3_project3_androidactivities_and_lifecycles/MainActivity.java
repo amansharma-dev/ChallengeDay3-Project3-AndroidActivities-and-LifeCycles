b549dc2b86b91d46a076efba9720ee785801167b
amansharma-dev <amansharma.11@hotmail.com>
2020-06-20 23:37:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/challengeday3_project3_androidactivities_and_lifecycles/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.challengeday3_project3_androidactivities_and_lifecycles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
